3a3f00ac0d8dd2dd56467bf7eee06caa657f9f64
SeungWuk Eun <seungwuk98@snu.ac.kr>
2024-12-24 14:59:36 +0900

    feat: implement algorithm test framework

--- test/benchmark/algorithm/test_algorithm.py
+++ test/benchmark/algorithm/test_algorithm.py
@@ -1,6 +1,8 @@
 import argparse 
 import subprocess
 import os
+import resource
+from enum import Enum, unique
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -34,6 +36,28 @@ def parse_args():
 
     return parser.parse_args()
 
+class Time:
+    @unique
+    class Row(Enum):
+        TIME = 0
+
+    @staticmethod
+    def run(command, *args, **kwargs):
+        command = ['time', '-f', "%M", *command]
+        before = resource.getrusage(resource.RUSAGE_CHILDREN)
+        result = subprocess.run(command, *args, **kwargs)
+        time = resource.getrusage(resource.RUSAGE_CHILDREN).ru_utime - before.ru_utime
+        if (result.returncode != 0):
+            timeParser = Time(time, None, result)
+        else:
+            timeParser = Time(time, result.stderr.decode('utf-8'),  result)
+        return timeParser
+
+    def __init__(self, time, time_str, result):
+        self.time = time * 1000
+        self.memory = int(time_str.strip()) if time_str else 0
+        self.result = result
+
 def main():
     args = parse_args() 
 
@@ -49,15 +73,16 @@ def main():
     files.sort()
 
     for input_file in files:
-        print("running test case: ", input_file)
+        print("====running test case: %d====" % input_file)
         input_file = os.path.join(case_dir, str(input_file))
         output_file = os.path.join(output_dir, os.path.basename(input_file))
         command = [bara_interpreter, bara_file]
-        result = subprocess.run(command, stdin = open(input_file, 'r'), stdout = open(output_file, 'w'), stderr = subprocess.PIPE)
-        if result.returncode != 0:
+        time = Time.run(command, stdin = open(input_file, 'r'), stdout = open(output_file, 'w'), stderr = subprocess.PIPE)
+      
+        if time.result.returncode != 0:
             print(f'Execution Error: {os.path.basename(input_file)} failed')
-            print(result.stderr.decode('utf-8'))
-            exit(result.returncode)
+            print(time.result.stderr.decode('utf-8'))
+            exit(time.result.returncode)
 
         answer_file = input_file + '.a'
 
@@ -66,7 +91,9 @@ def main():
         if result.returncode != 0:
             print(f'Checker Error: {os.path.basename(input_file)} failed')
             exit(result.returncode)
-        print(result.stderr.decode('utf-8'))
+        print(result.stderr.decode('utf-8').strip())
+        print(f'Execution Time: {time.time:.0f}ms')
+        print(f'Memory Usage: {int(time.memory)}KB')
 
 if __name__ == '__main__':
     main()
